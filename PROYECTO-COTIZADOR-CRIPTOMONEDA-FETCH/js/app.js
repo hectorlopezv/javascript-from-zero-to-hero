0532c117feb2bb37f8694772239135cd62a53a09
Hector Lopez <hectorvmlopez@gmail.com>
2020-09-15 21:32:00 -0500

    adding bitoncoing api

--- PROYECTO-COTIZADOR-CRIPTOMONEDA-FETCH/js/app.js
+++ PROYECTO-COTIZADOR-CRIPTOMONEDA-FETCH/js/app.js
@@ -2,7 +2,7 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
 const monedaSelect = document.querySelector('#moneda');
 const formulario = document.querySelector('#formulario');
-
+const resultado = document.querySelector('#resultado');
 const objBusqueda ={ 
     moneda:'',
     criptomoneda: ''
@@ -26,10 +26,72 @@ function submitFormulario(e){
     const {moneda, criptomoneda} = objBusqueda;
     if(moneda==='' || criptomoneda===''){//validacion si los campos estan vacios
         monstrarAlerta('ambos campos son obligatorios');
+        return;
     }
 
     //consultar la API con los resultados
+    consultarAPI();
 }
+function consultarAPI(){
+    const {moneda, criptomoneda} = objBusqueda;
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+    
+    mostrarSpinner();
+    fetch(url)
+    .then(response => response.json())
+    .then(cotizacion =>{
+        
+        monstrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+    })
+}
+function mostrarSpinner(){
+    limpiarHTML();
+    console.log('entro al spinner');
+    const btn = document.createElement('div');
+    btn.classList.add('spinner');
+    btn.innerHTML = `
+    <div class="bounce1"></div>
+    <div class="bounce2"></div>
+    <div class="bounce3"></div>`;
+    resultado.appendChild(btn);
+}
+function monstrarCotizacionHTML(cotizacion){
+    limpiarHTML();
+    console.log(cotizacion);
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = cotizacion;
+
+    const precio = document.createElement('p');
+    precio.classList.add('precio');
+    precio.innerHTML = `El Precio es: <span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement('p');
+    precioAlto.innerHTML = `<p>Precio más alto del día <span>${HIGHDAY}</span>`;
+
+    const precioBajo = document.createElement('p');
+    precioBajo.innerHTML = `<p>Precio más bajo del día <span>${LOWDAY}</span>`;
+
+    const ultimasHoras = document.createElement('p');
+    ultimasHoras.innerHTML = `<p>Variación últimas 24 horas <span>${CHANGEPCT24HOUR}%</span>`;
+
+    const ultimaActualización = document.createElement('p');
+    ultimaActualización.innerHTML = `<p>Última Actualización <span>${LASTUPDATE}</span>`;
+
+
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimaActualización);
+
+}
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
 
 function monstrarAlerta(mensaje){
     console.log(mensaje);
@@ -57,7 +119,7 @@ function consultarCriptomonedas(){
 
 function selectCriptomonedas(criptomonedas){
     criptomonedas.forEach(cripto => {
-        console.log(cripto);
+        //console.log(cripto);
         const {FullName, Name} = cripto.CoinInfo;
 
         const option = document.createElement('option');
