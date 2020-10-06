9a26c6fe5de7ef62cef07ee919870ee461dc5280
Hector Lopez <hectorvmlopez@gmail.com>
2020-10-06 05:01:36 -0500

    adding missing files

--- API-express-MongoDB/frontend/src/componentes/Error.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-
-const Error = () => {
-    return (  <h1>Error Page</h1>);
-}
- 
-export default Error;
--- API-express-MongoDB/frontend/src/config/axios.js
+++ API-express-MongoDB/frontend/src/config/axios.js
@@ -1,11 +1,6 @@
-//importamos axios
-// y creamos un cliente axios
 import axios from 'axios';
 
-// ponemos una url base para todos los endpoint
-//crea como un blueprint en la url
-
-const clienteAxios =  axios.create({
+const clienteAxios = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL
 });
 
